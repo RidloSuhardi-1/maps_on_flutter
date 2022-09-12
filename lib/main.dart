ddc6d17f3795e230f2926d9bb5d11b3cb296f4e4
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-09-12 11:25:22 +0700

    tugas

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,10 @@ class _MyAppState extends State<MyApp> {
   late GoogleMapController _mapController;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _myHome = const LatLng(-7.1433472, 112.6124914);
+
+  final Marker _myHomeMark = Marker(
+      markerId: MarkerId('My Home'), position: LatLng(-7.1433472, 112.6124914));
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
@@ -30,15 +34,18 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Maps sample App'),
+          title: const Text('Omahku'),
           backgroundColor: Colors.greenAccent,
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
+            target: _myHome,
             zoom: 11.0,
           ),
+          markers: {
+            _myHomeMark,
+          },
         ),
       ),
     );
